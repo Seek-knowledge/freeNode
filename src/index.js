ad78b9c6c8bc130d4bd8f627fd4ba3647a9ec0ee
Seek-knowledge <1402245414@qq.com>
2024-12-09 18:47:40 +0800

    Add files via upload
    添加node脚本生成文件

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+// 获取指定网络文件的文本
+async function getGithubusercontent(path) {
+    return new Promise((resolve, reject) => {
+        if (!path) {
+            return;
+        }
+        fetch(path)
+            .then(res => {
+                return res?.text?.();
+            })
+            .then(text => {
+                return resolve(text);
+            })
+            .catch(e => {
+                return reject(e);
+            })
+    })
+}
+
+// 提取链接
+function matchUrl(text) {
+    const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
+    const urls = text.match(urlPattern);
+    return urls || [];
+}
+
+// 处理文本
+function handleText(text) {
+    var reg = new RegExp(/v2rayN订阅链接\W+\S+/ig);
+    const result = text.match(reg);
+    return result?.[0] ? matchUrl(result[0]) : '';
+}
+
+// 初始化
+async function init() {
+    getGithubusercontent("https://raw.githubusercontent.com/mksshare/mksshare.github.io/refs/heads/main/README.md")
+        .then(text => {
+            const url = handleText(text)?.[0];
+            fs.writeFileSync(path.resolve('./url.txt'), url, 'utf-8')
+        })
+}
+
+init();
+
+
